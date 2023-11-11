95c55ba0df1bf42c92359eb1c98afa27d9c254a0
sakurakotubaki <gitonly543@gmail.com>
2023-11-11 18:49:50 +0900

    Initial Commit

---
+++ RestAPI/RestAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestAPIApp.swift
+//  RestAPI
+//
+//  Created by 橋本純一 on 2023/11/11.
+//
+
+import SwiftUI
+
+@main
+struct RestAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
